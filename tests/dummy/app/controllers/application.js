4547e99b70fd08d82e1a9567641089f8f6d34c18
Andrew Dixon <dixon@atomicbroadcast.net>
2016-02-07 22:16:56 -0800

    Todo Model
    * display on Application Route

---
+++ tests/dummy/app/controllers/application.js
@@ -0,0 +1,32 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend(
+  {
+    title: null,
+
+    completedTodos: Ember.computed.filterBy('model', 'isCompleted'),
+
+    completed: Ember.computed('completedTodos', function () {
+      return this.get('completedTodos.length');
+    }),
+
+    remaining: Ember.computed('completed', function () {
+      return this.get('model.length') - this.get('completed');
+    }),
+
+    s: Ember.computed('completed', function () {
+      return this.get('remaining') === 1 ? '' : 's';
+    }),
+
+    actions: {
+      createTodo(title) {
+        this.set('title', '');
+        this.store.createRecord('todo', {title: title.target.value});
+      },
+
+      hi(todo) {
+        todo.toggleProperty('isComplete');
+      }
+    }
+  }
+);
